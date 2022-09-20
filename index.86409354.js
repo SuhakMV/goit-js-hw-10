5297024a52d8c351ff382b57442f17ed5d17ac28
SuhakMV <103995334+SuhakMV@users.noreply.github.com>
2022-09-20 18:52:25 +0000

    Deploying to gh-pages from @ SuhakMV/goit-js-hw-10@1c8d05ac5f135d9d21d2c46241dc82eb3d2449c6 🚀

--- index.86409354.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/uk").then((e=>e.json())).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.86409354.js.map
--- index.b70e174c.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/uk").then((function(n){return n.json()})).then((function(n){console.log(n)}));
-//# sourceMappingURL=index.b70e174c.js.map
